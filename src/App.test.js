2e32b44a9fd06621c86b64986286c7da4cabc2b6
Evgenia <evgenianoidou@gmail.com>
2020-05-12 21:27:34 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from "./App";
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  const mockFunction = jest.fn((arg1, arg2) => arg1);
+  //const mockFunction = jest.fn((arg1, arg2) => arg1);
   expect(linkElement).toBeInTheDocument();
-  expect(mockFunction).toHaveBeenCalledTimes(0);
+  //expect(mockFunction).toHaveBeenCalledTimes(0);
 });
