690fb4c102c9e0332ea09b70ace59dbf0e53bb45
Evgenia <evgenianoidou@gmail.com>
2020-05-12 21:18:43 +0100

    Added files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
+  const mockFunction = jest.fn((arg1, arg2) => arg1);
   expect(linkElement).toBeInTheDocument();
+  expect(mockFunction).toHaveBeenCalledTimes(0);
 });
